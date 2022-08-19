5c09d6ab9d31a02b8f21d71b9d79bb9d03a239ff
RyanBreathings <ryan@breathings.co.kr>
2022-08-20 03:32:44 +0900

    fix go path

--- src/main.go
+++ src/main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"fmt"
 	"github.com/labstack/echo"
+	awsInit "main/aws"
 	"net/http"
 )
 
 func main() {
-	fmt.Println("test")
+	if err := awsInit.InitAws("ap-northeast-2"); err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
